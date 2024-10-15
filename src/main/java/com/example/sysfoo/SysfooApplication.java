1bdd3c261d728ae9e8be06904784cb3bf343f931
Gourav Shah <gs@initcron.org>
2024-10-15 12:36:59 +0530

    Update SysfooApplication.java

--- src/main/java/com/example/sysfoo/SysfooApplication.java
+++ src/main/java/com/example/sysfoo/SysfooApplication.java
@@ -4,10 +4,5 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SysfooApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SysfooApplication.class, args);
-	}
-
+public class Sywe
 }
